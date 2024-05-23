19018210bb70ee7331a6e2e17131b35437df2495
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-23 13:40:32 -0400

    feat: create handleclick and handlereset functions to keep track of player actions

--- matching-game/src/App.jsx
+++ matching-game/src/App.jsx
@@ -4,23 +4,44 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- const [images, setImages] = useState([
-  {id: 1, clicked: "false"}, {id: 2, clicked: "false"}, {id: 3, clicked: "false"}, {id: 4, clicked: "false"}, {id: 5, clicked: "false"},
-  {id: 6, clicked: "false"}, {id: 7, clicked: "false"}, {id: 8, clicked: "false"}, {id: 9, clicked: "false"}, {id: 10, clicked: "false"}
- ]);
+  const [wins, setWins] = useState(0);
+  const [images, setImages] = useState([
+    {id: 1, clicked: false}, {id: 2, clicked: false}, {id: 3, clicked: false}, {id: 4, clicked: false}, {id: 5, clicked: false},
+    {id: 6, clicked: false}, {id: 7, clicked: false}, {id: 8, clicked: false}, {id: 9, clicked: false}, {id: 10, clicked: false}
+  ]);
 
- const imageList = ["https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400",
- "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400"
- ];
-  
- const handleClick = (image) => {
-  console.log("hi");
- }
- 
+  const imageList = ["https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400",
+  "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400", "https://placehold.co/600x400"
+  ];
+    
+  const handleClick = (image, id) => {
+    if (image.clicked == true){
+      setWins(0);
+      handleReset();
+    }
+    else {
+      setWins(wins+1);
+      setImages(images.map(image => {
+        if (image.id == id){
+          return { ...image, clicked: true}
+        }
+        else{
+          return image;
+        }
+      }))
+    }
+   }
+   
+   const handleReset = () => {
+    setImages(images.map(image => {
+      return { ...image, clicked: false}
+    }))
+   }
   return (
     <div className="flex flex-row flex-wrap">
+      <p>{wins}</p>
       {images.map((image) => (
-        <div key={image.id} className="block w-1/5 h-40 border-4" onClick={() => handleClick(image)}>
+        <div key={image.id} className="block w-1/5 h-40 border-4" onClick={() => handleClick(image, image.id)}>
           <img src={imageList[image.id-1]}></img>
         </div>
       ))}
