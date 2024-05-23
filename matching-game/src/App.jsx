f13804c2a1cb19242b53b68bb5d0254ce87a051f
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-23 10:19:10 -0400

    feat: add image containers

--- matching-game/src/App.jsx
+++ matching-game/src/App.jsx
@@ -3,16 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const clickHandler = (event) => {
-
-}
 function App() {
- 
+ const [images, setImages] = useState([
+  {id: 1, clicked: "false"}, {id: 2, clicked: "false"}, {id: 3, clicked: "false"}, {id: 4, clicked: "false"}, {id: 5, clicked: "false"},
+  {id: 6, clicked: "false"}, {id: 7, clicked: "false"}, {id: 8, clicked: "false"}, {id: 9, clicked: "false"}, {id: 10, clicked: "false"}
+ ]);
   
   return (
-    <>
-      
-    </>
+    <div className="flex flex-row flex-wrap">
+      {images.map((image) => (
+        <div className="block w-1/5 h-40 border-4 ">
+          
+        </div>
+      ))}
+    </div>
   )
 }
 
