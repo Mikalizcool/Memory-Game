0e682314b8aef67c325eab5d43eafbab14d640a2
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-23 09:35:35 -0400

    refactor: remove unused code

--- matching-game/src/App.jsx
+++ matching-game/src/App.jsx
@@ -1,27 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 const clickHandler = (event) => {
-  
+
 }
 function App() {
-  const [count, setCount] = useState(0)
-  const boxes = [1, 2, 3, 4, 5];
+ 
+  
   return (
     <>
-      <div>
-        <div className="flex flex-row flex-wrap">
-          {boxes.map((box) => {
-            return (
-              <div key={box} className=" cursor-pointer m-4 border-solid border-4 w-80 h-80 border-black flex flex-wrap flex-row" onClick={clickHandler} >
-                {box}
-              </div>
-            )
-          })}
-        </div>
-      </div>
       
     </>
   )
