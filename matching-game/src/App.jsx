de4a5fa71d0287a1bb96e47bd6824d69cab9ceac
Mikalizcool <78291922+Mikalizcool@users.noreply.github.com>
2024-05-24 10:29:44 -0400

    feat: add names to characters

--- matching-game/src/App.jsx
+++ matching-game/src/App.jsx
@@ -7,9 +7,9 @@ import Header from './Header'
 function App() {
   const [wins, setWins] = useState(0);
   const [images, setImages] = useState([
-    {id: 1, clicked: false}, {id: 2, clicked: false}, {id: 3, clicked: false}, {id: 4, clicked: false}, {id: 5, clicked: false},
-    {id: 6, clicked: false}, {id: 7, clicked: false}, {id: 8, clicked: false}, {id: 9, clicked: false}, {id: 10, clicked: false},
-    {id: 11, clicked: false}, {id: 12, clicked: false}
+    {id: 1, clicked: false, name: "Morty"}, {id: 2, clicked: false, name: "Rick"}, {id: 3, clicked: false, name: "Jerry"}, {id: 4, clicked: false, name: "Summer"}, {id: 5, clicked: false, name: "Beth"},
+    {id: 6, clicked: false, name: "Jessica"}, {id: 7, clicked: false, name: "Birdperson"}, {id: 8, clicked: false, name: "Squanchy"}, {id: 9, clicked: false, name: "Miggs"}, {id: 10, clicked: false, name: "Ethan"},
+    {id: 11, clicked: false, name: "Lady Katana"}, {id: 12, clicked: false, name: "Vance"}
   ]);
 
   const imageList = ["https://comicvine.gamespot.com/a/uploads/square_small/6/66303/4469088-tumblr_inline_n0aleph3fl1r8rr6o.jpg", "https://comicvine.gamespot.com/a/uploads/square_small/6/66303/4469093-screen%20shot%202015-03-25%20at%205.13.24%20pm%20copy.jpg", "https://comicvine.gamespot.com/a/uploads/square_small/6/66303/4472081-tumblr_n04lnrmooh1rfbbc6o1_1280.jpg", "https://comicvine.gamespot.com/a/uploads/square_small/6/66303/4472085-summer_in_ricksy_business.jpg", "https://comicvine.gamespot.com/a/uploads/square_small/6/66303/4472083-vlcsnap-2015-01-31-18h46m55s179.jpg",
@@ -48,6 +48,7 @@ function App() {
         {images.map((image) => (
           <div key={image.id} className="characterContainer" onClick={() => handleClick(image, image.id)}>
             <img className="character" src={imageList[image.id-1]}></img>
+            <p>{image.name}</p>
           </div>
         ))}
       </div>
--- matching-game/src/Header.jsx
+++ matching-game/src/Header.jsx
@@ -1,9 +1,9 @@
 const Header = ({wins}) => {
     return (
         <>
-            <h1>Rick and Morty Matching Game</h1>
+            <h1 className="title">Rick and Morty Matching Game</h1>
             <p>Get points for clicking on an image, but don't click on any more than once!</p>
-            <p>Wins: {wins}</p>
+            <p>Score: {wins}</p>
         </>
     )
 }
